1c0a6f9f07e9146875d9309275f16ebb613abfac
geniuh <geniuh1@gmail.com>
2024-03-03 11:07:38 -0500

    pruebas

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,10 @@
 
-import Routes from './presentation/routes/routes'
+import {Server, Controllers, Routes} from './presentation/routes/routes'
 import express, {} from 'express'
 const app= express();
 
-const routes= new Routes(app,3000);
-
-routes.routesn();
-routes.run();
+const server = new Server(app,3000);
+server.run()
+const controller = [new Controllers('/hola',()=>console.log("funciona"),()=>console.log("no funciona"))];
+const routes = new Routes(app,controller);
+routes.routes();
--- src/presentation/routes/routes.ts
+++ src/presentation/routes/routes.ts
@@ -1,27 +1,68 @@
 import {Response,Express, Request} from 'express'
 
+interface call{
+
+}
+export class Routes {
+    private app:Express;
+    private controllers:Controllers[];
+
+    constructor(app:Express, controllers:Controllers[]){
+        this.controllers=controllers;
+        this.app=app;
+    }
+
+    public routes(): void {
+        this.controllers.map((controller)=>{
+                this.app.get(controller.getRute(), controller.getHandler());
+            }
+        )
+    }
+}
+
+export class Controllers {
+    private rute:string;
+    private body:Function;
+    private erro:Function;
+
+    constructor (rute:string, body:Function,erro:Function){
+        this.rute=rute;
+        this.body=body;
+        this.erro=erro;
+    }
+
+    public getRute(){
+        return this.rute;
+    }
+    public getHandler(){
+        return async (req: Request, res: Response) => {
+        try {
+            this.body();
+            return res.status(200).json({ message: "Solicitud procesada con exito" })
+        }
+        catch {
+            this.erro();
+            return res.status(500).json({ message: 'Ha ocurrido un error en el servidor' })
+        }
+    }
+    }
+}
 
 
-class Routes {
+export class Server {
     private app:Express;
     private port:number;
     constructor(app:Express, port:number){
         this.app=app;
         this.port=port;
-
     }
-
-    public routesn(): void {
-        this.app.get('/hola', function (req:Request,res:Response){
-            res.send("hola");
-        });
-        
-    }
-
     public run():void{
-        this.app.listen(this.port);
+        this.app.listen(this.port, () => {
+            console.log(`Server running on port ${this.port}`);
+
+        }).on('error', (error: any) => {
+            console.error(`Failed to start server: ${error}`);
+            process.exit(1); // Exit the process if server fails to start
+        });
     }
-
 }
-
-export default Routes;
